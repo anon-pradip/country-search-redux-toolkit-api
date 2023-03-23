b8fd50ee734c0420340986c2bd9d63b0f018da71
anon-pradip <pradipsubedi360@gmail.com>
2023-03-23 20:01:50 +0545

    make single country name available

--- src/features/countries/countryActions.js
+++ src/features/countries/countryActions.js
@@ -7,14 +7,13 @@ export const showAllCountries = createAsyncThunk("countries/showAll", async (_, 
   try {
     const response = await axios.get("https://restcountries.com/v3.1/all")
     return response.data
-
   } catch (error) {
     const message = (error.response && error.response.data) || error.message
     return thunkAPI.rejectWithValue(message)
   }
 })
 
-export const searchByCode = createAsyncThunk("countries/showCountryByCode", async (code, thunkAPI) => {
+export const searchByCode = createAsyncThunk("countries/searchByCode", async (code, thunkAPI) => {
   try {
     const response = await axios.get(`https://restcountries.com/v3.1/alpha/${code}
     `)
--- src/pages/SIngleCountry.js
+++ src/pages/SIngleCountry.js
@@ -8,23 +8,45 @@ import { searchByCode } from '../features/countries/countryActions'
 const SingleCountry = () => {
   const { loading, error, countrySearched } = useSelector(state => state.countries)
   const dispatch = useDispatch();
-  console.log(useParams())
   const { code } = useParams()
-  console.log(code)
 
-  // useEffect(() => {
-  //   dispatch(searchByCode())
-  // })
+  useEffect(() => {
+    if (code) {
+      dispatch(searchByCode(code.toLowerCase()))
+    }
+    if (error) { console.log(error) }
+  }, [dispatch, code, error])
 
   return (
-    <div className=' py-7'>
-      <div className=' text-center'>
+    <div className=' py-7 px-4'>
+      <Link to="/" className=' flex justify-center items-center'>
         <button
           type="button"
-          className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 spacefocus:ring-offset-2"
         >
           Go Back
         </button>
+      </Link>
+
+      {/* main section */}
+      <div className=''>
+        {countrySearched?.length >= 1 ? (
+          <>
+            <div className='flex flex-col space-y-3 space-x-0 md:flex-row md:-y-0 md:space-x-4'>
+              {/* IMAGE */}
+              <div className='w-max'>
+                <img src={countrySearched[0].flags.png} alt={countrySearched[0].name} className='object-contain w-48' />
+              </div>
+
+              {/* CONTENT */}
+              <div className=' flex'>
+                <h1>{countrySearched[0].name.official}</h1>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>Sorry! No details found.</div>
+        )}
       </div>
     </div>
   )
